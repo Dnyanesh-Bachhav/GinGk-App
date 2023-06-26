a8a3f4b1d0eabffc9a61306041c1640ca2a33495
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-06-27 00:05:38 +0530

    Splash screen added

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button } from '@rneui/themed';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World...!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Hello World...!!!</Text>
+        <Button title="Solid" />
+        <Button
+              title={'React Native Elements'}
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+            />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
